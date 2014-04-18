3d32b76a938c97b6d9a1b16b6b28bd89fc2f80f6
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 17:43:53 +0200

    Update ConversionTest.java

--- ConversionTest.java
+++ ConversionTest.java
@@ -2,15 +2,18 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+* The ConversionTest allows to test methods
+* @author Nabil HACHEM
+* @version 2.0
+* @since 2014-04-18
+*/
+
 public class ConversionTest extends TestCase{
 
-      /**
-      @author Nabil HACHEM
-      @version 2.0
-      @since 2014-04-18
-      */
+// This method allows to test the method convertir_unite
 
-public void testMultiplierUniteParDix() throws Exception {
+public void testConvertir_unite throws Exception {
 
 assertEquals(Conversion.convertir_unite());
 }
