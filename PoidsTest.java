f5396fd82a3cf8572b8123838da0031d42ff70d6
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:30:15 +0200

    Update PoidsTest.java

--- PoidsTest.java
+++ PoidsTest.java
@@ -4,6 +4,12 @@ import junit.framework.TestSuite;
 
 public class PoidsTest extends TestCase{
 
+		/**
+		@author Nabil HACHEM
+		@version 2.0
+		@since 2014-04-18
+		*/
+		
 	public void testAfficherUnitesPoids() throws Exception {
 	Poids p=new Poids();
 	assertEquals(p.afficherUnitesPoids(),"Tonne, Quintal, Kg, Hg, Dag, G, Dg, Cg, Mg ");
