71d95ad6af67146c131215b1500eba24cb837680
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 18:17:27 +0200

    Update MetriqueTest.java

--- MetriqueTest.java
+++ MetriqueTest.java
@@ -2,17 +2,21 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+* The MetriqueTest allows to test methods
+* @author Nabil HACHEM
+* @version 2.0
+* @since 2014-04-18
+*/
+
 public class MetriqueTest extends TestCase{
 
-		
-		/**
-		@author Nabil HACHEM
-		@version 2.0
-		@since 2014-04-18
-		*/
+	// This method allows to test the method afficherUnitesMetrique
 
 	public void testAfficherUnitesMetrique() throws Exception {
+		
 	Metrique m=new Metrique();
+	
 	assertEquals(m.afficherUnitesMetrique(),"Km, Hm, Dam, M, Dm, Cm, Mm, ");
 							
 							      }
