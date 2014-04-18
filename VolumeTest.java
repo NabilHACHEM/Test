58f4c0237704249ca04a1d310909adf6688298eb
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 18:43:47 +0200

    Update VolumeTest.java

--- VolumeTest.java
+++ VolumeTest.java
@@ -2,16 +2,21 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+* The VolumeTest allows to test methods
+* @author Nabil HACHEM
+* @version 2.0
+* @since 2014-04-18
+*/
+
 public class VolumeTest extends TestCase{
 
-		/**
-		@author Nabil HACHEM
-		@version 2.0
-		@since 2014-04-18
-		*/
+	// This method allows to test the method afficherUnitesVolume()
 
 	public void testAfficherUnitesVolume() throws Exception {
+		
 	Volume v=new Volume();
+	
 	assertEquals(v.afficherUnitesVolume(),"m3, dm3, cm3, l, dl, cl, ml, ");
 							
 							      }
