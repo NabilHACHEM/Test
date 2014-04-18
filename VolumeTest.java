e5d6a6b04a5c177063b41977b0105e0e170022cf
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:32:01 +0200

    Update VolumeTest.java

--- VolumeTest.java
+++ VolumeTest.java
@@ -4,6 +4,12 @@ import junit.framework.TestSuite;
 
 public class VolumeTest extends TestCase{
 
+		/**
+		@author Nabil HACHEM
+		@version 2.0
+		@since 2014-04-18
+		*/
+
 	public void testAfficherUnitesVolume() throws Exception {
 	Volume v=new Volume();
 	assertEquals(v.afficherUnitesVolume(),"m3, dm3, cm3, l, dl, cl, ml, ");
