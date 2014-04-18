4dedad840ff6bdf42b3dd586cce6c678009fb729
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:31:28 +0200

    Update TempsTest.java

--- TempsTest.java
+++ TempsTest.java
@@ -4,6 +4,12 @@ import junit.framework.TestSuite;
 
 public class TempsTest extends TestCase{
 
+		/**
+		@author Nabil HACHEM
+		@version 2.0
+		@since 2014-04-18
+		*/
+		
 	public void testAfficherUnitesTemps() throws Exception {
 	Temps t=new Temps();
 	assertEquals(t.afficherUnitesTemps(),"Annee, Mois, Semaine, Jour, Heure, Minute, Seconde, Milliseconde, Microseconde, Nanoseconde, ");
