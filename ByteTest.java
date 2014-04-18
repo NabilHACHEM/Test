980882d9694f179170fb120613f19ce959ce16e6
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 17:25:19 +0200

    Update ByteTest.java

--- ByteTest.java
+++ ByteTest.java
@@ -2,14 +2,17 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+ * The ByteTest allows to test methods 
+ * @author Nabil HACHEM
+ * @version 2.0
+ * @since 2014-04-18
+*/
+
 public class ByteTest extends TestCase{
 	
 	
-		/**
-		@author Nabil HACHEM
-		@version 2.0
-		@since 2014-04-18
-		*/
+	// This method allows to test the method afficherUnitesByte()
 
 	public void testAfficherUnitesByte() throws Exception {
 	Byte b=new Byte();
