15d0d2fca9dc1666bbd51c5c2c3ba162c85f5c9c
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 17:50:59 +0200

    Update FinanceTest.java

--- FinanceTest.java
+++ FinanceTest.java
@@ -2,13 +2,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+* The FinanceTest allows to test methods
+* @author Nabil HACHEM
+* @version 2.0
+* @since 2014-04-18
+*/
+
 public class FinanceTest extends TestCase{
 	
-		/**
-		@author Nabil HACHEM
-		@version 2.0
-		@since 2014-04-18
-		*/
+	// This method allows to test the method afficherUnitesFinance
 
 	public void testAfficherUnitesFinane() throws Exception {
 	Finance f =new Finance();
