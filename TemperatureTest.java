d05ac1563de6fc907479db85eeb685fdaccaea1d
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:30:54 +0200

    Update TemperatureTest.java

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -4,6 +4,12 @@ import junit.framework.TestSuite;
 
 public class TemperatureTest extends TestCase{
 
+		/**
+		@author Nabil HACHEM
+		@version 2.0
+		@since 2014-04-18
+		*/
+
 	public void testAfficherUnitesTemperature() throws Exception {
 	Temperature t=new Temperature();
 	assertEquals(t.afficherUnitesTemperature(),"Celsius, Kelvin, Newton, ");
