aaaeede14c4de8d817859e0cea7c8f8c1cd11077
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 15:55:38 +0200

    Create ConversionTest.java

---
+++ ConversionTest.java
@@ -0,0 +1,12 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ConversionTest extends TestCase{
+
+public void testMultiplierUniteParDix() throws Exception {
+
+assertEquals(Conversion.convertir_unite());
+}
+ 
+}
