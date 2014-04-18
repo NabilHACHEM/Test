b42224c9fe25494b0bacc039eb4a3c2ecabc0c10
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:27:45 +0200

    Update FormuleTest.java

--- FormuleTest.java
+++ FormuleTest.java
@@ -3,6 +3,13 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class FormuleTest extends TestCase{
+ 
+      /**
+      @author Nabil HACHEM
+      @version 2.0
+      @since 2014-04-18
+      */
+ 
 public void testMultiplierUniteParDix() throws Exception {
  
 assertEquals(230.0,Formule.multiplierUniteParDix("km","hm",23.0));
