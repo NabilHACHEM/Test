44f58f84f2731b87961553a458aac9e1d26b7438
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 17:41:20 +0200

    Update Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -1,23 +1,30 @@
+/**
+* The Conversion class is a class which converts units of a system to another units
+* @author Nabil HACHEM
+* @version 2.0
+* @since 2014-04-18
+*/
+
 public class Conversion {
 			
-				/**
-				   @author Nabil HACHEM
-				   @version 2.0
-			           @since 2014-04-18
-				  */
+			/**These attributs are private and two of them are in type String and the one another in type float
+			  * unite_origine represents the caracter of the first unit
+			  * unite_cible represents the caracter of the second unit
+			  * valeur_unite is the number of the unit
+			*/
 			
-			//Attributs	
-		
 			private String unite_origine;
 			private String unite_cible;
 			private float valeur_unite;
 			
-			//Constructeurs
+			//This constructor allows to initialized the method convertir_unite()
 			
 			public Conversion() {
 				super();
 				convertir_unite();
 			}
+			
+			//This constructor allows to initialized all the attributs
 			
 			public Conversion(String unit_orig,String unit_cib, float val_unit){
 				this.unite_cible=unit_cib;
@@ -26,7 +33,7 @@ public class Conversion {
 				//convertir_unite();
 			}
 			
-			//Getters & setters
+			//Getters & setters for all attributs
 			
 			public String getUnite_origine() {
 				return unite_origine;
@@ -53,14 +60,14 @@ public class Conversion {
 			}
 			
 			
-			//M�thode permettant de convertir les unit�s
+			//Methode allows to convertles units
 			
 			
 			public void convertir_unite(){
 								
 				Formule f=new Formule();
 				
-			//Les formules permettant de multiplier les unit�s entre eux selon la valeur	
+			//Methods allows to multiply units between them  according to the number	
 				
 				f.multiplierUniteParUn(getUnite_origine(), getUnite_cible(), getValeur_unite());
 				f.multiplierUniteParDix(getUnite_origine(),getUnite_cible(),getValeur_unite());
@@ -73,7 +80,7 @@ public class Conversion {
 				f.multiplierUniteParCentMillion(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.multiplierUniteParUnMilliard(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 
-			//Les formules permettant de diviser les unit�s entre eux selon la valeur	
+			//Methods allows to divide units between them  according to the number	
 				
 				f.diviserUniteParUn(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.diviserUniteParDix(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
@@ -84,32 +91,32 @@ public class Conversion {
 				f.diviserUniteParUnMillion(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.diviserUniteParUnMillion(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				
-			//Les formules permettant de convertir les devises entre eux
+			//Methods allows to convert currencies 
 				
 				f.euro(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.dollar(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.Pound(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.Yen(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				
-			//Les formules permettant de convertir les unit�s de temps entre eux
+			//Methods allows to convert units of time between them  according to the number
 				
 				f.heure(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.minute(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 
-			//Les formules permettant de convertir les unit�s de vitesse entre eux	
+			//Methods allows to convert units of speed between them  according to the number	
 				
 				f.kmph(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.mps(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.milesph(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				
-			//Les formules permettant de convertir les unit�s de temp�rature entre eux	
+			//Methods allows to convert units of temperature between them  according to the number	
 				
 				f.celsius(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.kelvin(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.newton(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 			
-			//Les formules permettant de convertir les unit�s de byte entre eux
-				
+			////Methods allows to convert units of system byte between them  according to the number
+			
 				f.bit(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.octet(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
 				f.ko(this.getUnite_origine(), this.getUnite_cible(), this.getValeur_unite());
