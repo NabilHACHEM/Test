0dcd91134ab26c7fb0e4a9bc654c2a13f1a48a71
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:12:23 +0200

    Update Byte.java

--- Byte.java
+++ Byte.java
@@ -2,6 +2,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Byte {
+			
+			/**
+			@author  Nabil HACHEM
+			@version 2.0
+ 			@since   2014-04-18
+			*/
+		
+	
 	
 			//Attributs	
 			
