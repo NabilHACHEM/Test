6246cc5467e16989725863278d814febcfbf71a2
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:27:13 +0200

    Update FinanceTest.java

--- FinanceTest.java
+++ FinanceTest.java
@@ -3,6 +3,12 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class FinanceTest extends TestCase{
+	
+		/**
+		@author Nabil HACHEM
+		@version 2.0
+		@since 2014-04-18
+		*/
 
 	public void testAfficherUnitesFinane() throws Exception {
 	Finance f =new Finance();
