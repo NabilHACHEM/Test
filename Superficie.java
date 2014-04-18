869ed831ad62f2d0a03d1628fda3eae7b1a01790
NabilHACHEM <nayelhashim@hotmail.com>
2014-04-18 16:16:05 +0200

    Update Superficie.java

--- Superficie.java
+++ Superficie.java
@@ -3,6 +3,12 @@ import java.util.Iterator;
 
 public class Superficie {
 	
+				/**
+				   @author Nabil HACHEM
+				   @version 2.0
+			           @since 2014-04-18
+				  */
+	
 			//Attributs
 			
 			private String km2;
